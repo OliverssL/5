06dc14e5e27acdc921cbf30934e2ddd6900e5f72
OliverssL <123443306+OliverssL@users.noreply.github.com>
2023-11-07 00:50:50 +0200

    Form 2 dizaina uzlabojums
    Form 2 dizains un kods priekš Datu pievienošanas

--- PrecuDatubaze_OL_ZV/Form2.Designer.cs
+++ PrecuDatubaze_OL_ZV/Form2.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Vards = new System.Windows.Forms.TextBox();
+            this.Uzvards = new System.Windows.Forms.TextBox();
+            this.Tel_numurs = new System.Windows.Forms.TextBox();
+            this.Pers_kods = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Pierakstīties = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vārds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(98, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzvārds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tel_numurs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pers_kods";
+            // 
+            // Vards
+            // 
+            this.Vards.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vards.Location = new System.Drawing.Point(237, 109);
+            this.Vards.Name = "Vards";
+            this.Vards.Size = new System.Drawing.Size(285, 38);
+            this.Vards.TabIndex = 4;
+            // 
+            // Uzvards
+            // 
+            this.Uzvards.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Uzvards.Location = new System.Drawing.Point(237, 167);
+            this.Uzvards.Name = "Uzvards";
+            this.Uzvards.Size = new System.Drawing.Size(285, 38);
+            this.Uzvards.TabIndex = 5;
+            // 
+            // Tel_numurs
+            // 
+            this.Tel_numurs.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tel_numurs.Location = new System.Drawing.Point(237, 215);
+            this.Tel_numurs.Name = "Tel_numurs";
+            this.Tel_numurs.Size = new System.Drawing.Size(285, 38);
+            this.Tel_numurs.TabIndex = 6;
+            // 
+            // Pers_kods
+            // 
+            this.Pers_kods.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pers_kods.Location = new System.Drawing.Point(237, 276);
+            this.Pers_kods.Name = "Pers_kods";
+            this.Pers_kods.Size = new System.Drawing.Size(285, 38);
+            this.Pers_kods.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(296, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 46);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Pieraksts";
+            // 
+            // Pierakstīties
+            // 
+            this.Pierakstīties.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pierakstīties.Location = new System.Drawing.Point(237, 338);
+            this.Pierakstīties.Name = "Pierakstīties";
+            this.Pierakstīties.Size = new System.Drawing.Size(285, 67);
+            this.Pierakstīties.TabIndex = 9;
+            this.Pierakstīties.Text = "Pierakstīties";
+            this.Pierakstīties.UseVisualStyleBackColor = true;
+            this.Pierakstīties.Click += new System.EventHandler(this.Pierakstīties_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pierakstīties);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Pers_kods);
+            this.Controls.Add(this.Tel_numurs);
+            this.Controls.Add(this.Uzvards);
+            this.Controls.Add(this.Vards);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Vards;
+        private System.Windows.Forms.TextBox Uzvards;
+        private System.Windows.Forms.TextBox Tel_numurs;
+        private System.Windows.Forms.TextBox Pers_kods;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Pierakstīties;
     }
 }
--- PrecuDatubaze_OL_ZV/Form2.cs
+++ PrecuDatubaze_OL_ZV/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace PrecuDatubaze_OL_ZV
 {
@@ -15,6 +16,27 @@ namespace PrecuDatubaze_OL_ZV
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Pierakstīties_Click(object sender, EventArgs e)
+        {
+            {
+                if (Vards.Text != "")
+                {
+                    SQLiteConnection sqlite_conn;
+                    sqlite_conn = CreateConnection();
+                    SQLiteCommand sqlite_cmd;
+                    sqlite_cmd = sqlite_conn.CreateCommand();
+                    sqlite_cmd.CommandText = "INSERT INTO Vārds (Vārds) VALUES('"Vards.Text + "');";
+                    sqlite_cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("Lūdzu ievadien jūsu Vārdu");
+                }
+
+
+            }
         }
     }
 }
